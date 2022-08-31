86046893c9791f31869e29977e4d348202fce9dd
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-08-31 10:54:09 +1000

    Added improvements to input validation

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Testing with user validation, basic CSS styling and preventDefault;
 30/08/2022 - Initial commit; validation and logic still to be implemented;
 
 31/08/2022 - Added first layer of validation and logic; 
+             Added prevent default to stop form from refreshing page and no input being displayed. 
+
+31/08/2022 - User validation added. Error message displayed in different if no input or if NaN is entered. 
+            If valid input is entered, message changes colour and displays result. 
--- index.js
+++ index.js
@@ -4,18 +4,17 @@ let submitBtn = document.querySelector('#submitBtn');
 let displayResult = document.querySelector('#displayResult');
 
 
-
-submitBtn.addEventListener('click', function (e){
-    validation(e);
-    });
-
+submitBtn.addEventListener('click', function (e){ validation(e) });
 
 function validation(e){
     e.preventDefault();
     if (userHeight.value == "" || userWeight.value == "") {
+        displayResult.style.backgroundColor = 'salmon';
         displayResult.innerHTML = "Cannot be blank - please enter something";
             }
+
     else if (isNaN(userHeight.value) == true || isNaN(userWeight.value) == true){
+        displayResult.style.backgroundColor = 'salmon';
         return displayResult.innerHTML = "And how do you expect me to calculate something that isn't a number?";
         }
     else {
@@ -27,8 +26,11 @@ function receiveInput(e){
 e.preventDefault();
 userHeight = userHeight.value;
 userWeight = userWeight.value;
+displayResult.style.backgroundColor = 'green';
 displayResult.innerHTML = `Your BMI is ${(userWeight / Math.sqrt(userHeight,2)).toFixed(2)}` ;
 }
+
+
 
 
 
