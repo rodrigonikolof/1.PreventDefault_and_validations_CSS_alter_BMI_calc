ee797a26a8862e8cec6ed614634951c36ed45524
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-03 20:28:39 +1000

    Fixed formula

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ let userWeight = document.querySelector("#userWeight");
 let submitBtn = document.querySelector('#submitBtn');
 let displayResult = document.querySelector('#displayResult');
 
-
 submitBtn.addEventListener('click', function (e){ validation(e) });
 
 function validation(e){
     e.preventDefault();
     if (userHeight.value == "" || userWeight.value == "") {
-        displayResult.style.backgroundColor = 'salmon';
+        // displayResult.style.backgroundColor = 'salmon';
+        displayResult.classList.add('error');
         displayResult.innerHTML = "Cannot be blank - please enter something";
             }
 
     else if (isNaN(userHeight.value) == true || isNaN(userWeight.value) == true){
-        displayResult.style.backgroundColor = 'salmon';
+        // displayResult.style.backgroundColor = 'salmon';
+        displayResult.classList.add('error');
         return displayResult.innerHTML = "And how do you expect me to calculate something that isn't a number?";
         }
     else {
@@ -27,7 +28,7 @@ e.preventDefault();
 userHeight = userHeight.value;
 userWeight = userWeight.value;
 displayResult.style.backgroundColor = 'green';
-displayResult.innerHTML = `Your BMI is ${(userWeight / Math.sqrt(userHeight,2)).toFixed(2)}` ;
+displayResult.innerHTML = `Your BMI is ${(userWeight / Math.pow(userHeight,2)).toFixed(2)}` ;
 }
 
 
